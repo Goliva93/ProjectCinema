2e3c95ffd753ca13038fa9150e7c79ab5c9f1c8c
G-Laptop <G-Laptop@DESKTOP-642IIKR>
2022-05-05 02:19:28 -0500

    implement interface movieCatalog

---
+++ ProjectCinema/src/pe/goliva/cinema/service/IMovieCatalog.java
@@ -0,0 +1,20 @@
+package pe.goliva.cinema.service;
+
+/**
+ *
+ * @author : Giomar Oliva
+ * @date : 5 may 2022
+ * 
+ */
+public interface IMovieCatalog {
+	String NAME_RECEARCH = "movies.txt";
+
+	void insertMovie(String nameMovie);
+
+	void listMovie();
+
+	void searchMovie(String search);
+
+	void createMovieCatalog();
+
+}
