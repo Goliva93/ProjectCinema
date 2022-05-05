ac942c0f342db531a70697f2f58b5c4839e53dbc
G-Laptop <G-Laptop@DESKTOP-642IIKR>
2022-05-04 21:36:28 -0500

    create Project

---
+++ ProjectCinema/src/pe/goliva/cinema/exceptions/DataAccessExc.java
@@ -0,0 +1,12 @@
+package pe.goliva.cinema.exceptions;
+
+/**
+ *
+ * @author	: Giomar Oliva
+ * @date	: 4 may 2022
+ * 
+ */
+@SuppressWarnings("serial")
+public class DataAccessExc extends RuntimeException {
+
+}
